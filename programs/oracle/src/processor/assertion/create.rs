a9d0c53ac5f2ab1211ba6448dc9c30c792ac950d
James Whaley <juicy66173@gmail.com>
2024-03-22 18:26:15 +0000

    Fix incorrect location for assertion create code

--- programs/oracle/src/processor/assertion/create.rs
+++ programs/oracle/src/processor/assertion/create.rs
@@ -1,46 +1,55 @@
 use solana_program::account_info::AccountInfo;
+use solana_program::clock::Clock;
 use solana_program::entrypoint::ProgramResult;
 use solana_program::program_error::ProgramError;
 use solana_program::pubkey::Pubkey;
+use solana_program::sysvar::Sysvar;
 
 use crate::cpi::spl::{CreateTokenAccount, TransferChecked};
-use crate::instruction::accounts::{Context, CreateRequestAccounts};
-use crate::instruction::CreateRequestArgs;
-use crate::state::{AccountSized, InitAccount, InitContext, InitRequest, Oracle, Request};
+use crate::error::OracleError;
+use crate::instruction::accounts::{Context, CreateAssertionAccounts};
+use crate::instruction::CreateAssertionArgs;
+use crate::state::{
+    AccountSized, Assertion, InitAccount, InitAssertion, InitContext, Request, RequestState,
+};
 use crate::{cpi, pda, utils};
 
 pub fn create<'a>(
     program_id: &'a Pubkey,
     accounts: &'a [AccountInfo<'a>],
-    args: CreateRequestArgs,
+    args: CreateAssertionArgs,
 ) -> ProgramResult {
-    let ctx = CreateRequestAccounts::context(accounts)?;
+    let ctx = CreateAssertionAccounts::context(accounts)?;
 
     match args {
-        CreateRequestArgs::V1 { .. } => create_v1(program_id, ctx, args),
+        CreateAssertionArgs::V1 { .. } => create_v1(program_id, ctx, args),
     }
 }
 
 fn create_v1(
     program_id: &Pubkey,
-    ctx: Context<CreateRequestAccounts>,
-    args: CreateRequestArgs,
+    ctx: Context<CreateAssertionAccounts>,
+    args: CreateAssertionArgs,
 ) -> ProgramResult {
-    let CreateRequestArgs::V1 { reward, timestamp, data } = args;
+    let CreateAssertionArgs::V1 { bond, value } = args;
 
-    let CreateRequestAccounts {
+    let CreateAssertionAccounts {
         oracle,
         request,
-        reward_mint,
-        reward_source,
-        reward_escrow,
-        creator,
+        assertion,
+        bond_mint,
+        bond_source,
+        bond_escrow,
+        governance_mint,
+        governance_source,
+        governance_escrow,
+        asserter,
         payer,
         token_program,
         system_program,
     } = ctx.accounts;
 
-    if !creator.is_signer || !payer.is_signer {
+    if !asserter.is_signer || !payer.is_signer {
         return Err(ProgramError::MissingRequiredSignature);
     }
 
@@ -49,30 +58,39 @@ fn create_v1(
 
     pda::oracle::assert_pda(oracle.key)?;
 
-    let request_index: u64;
-
-    // Step 1: Get and increment next request index.
-    {
-        let mut oracle = Oracle::from_account_info_mut(oracle)?;
-
-        request_index = oracle.next_index;
-
-        oracle.next_index = increment!(oracle.next_index)?;
-        oracle.save()?;
+    if bond < crate::MIN_BOND {
+        return Err(OracleError::InsufficientBond.into());
     }
 
-    // Step 2: Initialize `request` account.
-    {
-        let request_bump = pda::request::assert_pda(request.key, &request_index)?;
-        let signer_seeds = pda::request::seeds_with_bump(&request_index, &request_bump);
+    // TODO: Use marker accounts to check valid bond mints.
+    // TODO: Check governance mint address.
 
-        Request::try_init(InitRequest {
-            index: request_index,
-            creator: *creator.key,
-            reward,
-            reward_mint: *reward_mint.key,
-            timestamp,
-            data,
+    // Step 1: Update request state.
+    {
+        let request_address = request.key;
+
+        let mut request = Request::from_account_info_mut(request)?;
+
+        request.assert_pda(request_address)?;
+        request.assert_requested()?;
+
+        request.state = RequestState::Asserted;
+        request.save()?;
+    }
+
+    // Step 2: Initialize `assertion` account.
+    {
+        let assertion_bump = pda::assertion::assert_pda(assertion.key, request.key)?;
+        let signer_seeds = pda::assertion::seeds_with_bump(request.key, &assertion_bump);
+
+        Assertion::try_init(InitAssertion {
+            request: *request.key,
+            governance: crate::GOVERNANCE_BOND,
+            bond,
+            bond_mint: *bond_mint.key,
+            assertion_timestamp: Clock::get()?.unix_timestamp,
+            asserter: *asserter.key,
+            asserted_value: value,
         })?
         .save(InitContext {
             account: request,
@@ -83,20 +101,20 @@ fn create_v1(
         })?;
     }
 
-    // Step 3: Transfer reward to escrow.
-    if reward > 0 {
-        let mint_decimals = cpi::spl::get_decimals(reward_mint)?;
+    // Step 3: Transfer bond to escrow.
+    {
+        let mint_decimals = cpi::spl::get_decimals(bond_mint)?;
 
-        // Step 3.1: Initialize `reward_escrow` account.
+        // Step 3.1: Initialize `bond_escrow` account.
         {
-            let reward_bump = pda::reward::assert_pda(reward_escrow.key, request.key)?;
-            let signer_seeds = pda::reward::seeds_with_bump(request.key, &reward_bump);
+            let bond_bump = pda::assert_bond::assert_pda(bond_escrow.key, request.key)?;
+            let signer_seeds = pda::assert_bond::seeds_with_bump(request.key, &bond_bump);
 
             cpi::spl::create_token_account(
                 request.key,
                 CreateTokenAccount {
-                    account: reward_escrow,
-                    mint: reward_mint,
+                    account: bond_escrow,
+                    mint: bond_mint,
                     payer,
                     token_program,
                     system_program,
@@ -105,22 +123,61 @@ fn create_v1(
             )?;
         }
 
-        // Step 3.2: Transfer reward from `reward_source` to `reward_escrow`.
+        // Step 3.2: Transfer bond from `bond_source` to `bond_escrow`.
         cpi::spl::transfer_checked(
-            reward,
+            bond,
             mint_decimals,
             TransferChecked {
-                source: reward_source,
-                destination: reward_escrow,
-                mint: reward_mint,
-                authority: creator,
+                source: bond_source,
+                destination: bond_escrow,
+                mint: bond_mint,
+                authority: asserter,
                 token_program,
             },
             &[],
         )?;
     }
 
-    // TODO: Emit an event for the assertion?
+    // Step 4: Transfer governance to escrow.
+    {
+        let mint_decimals = cpi::spl::get_decimals(governance_mint)?;
+
+        // Step 3.1: Initialize `governance_escrow` account.
+        {
+            let governance_bump =
+                pda::assert_governance::assert_pda(governance_escrow.key, request.key)?;
+            let signer_seeds =
+                pda::assert_governance::seeds_with_bump(request.key, &governance_bump);
+
+            cpi::spl::create_token_account(
+                request.key,
+                CreateTokenAccount {
+                    account: governance_escrow,
+                    mint: governance_mint,
+                    payer,
+                    token_program,
+                    system_program,
+                },
+                &[&signer_seeds],
+            )?;
+        }
+
+        // Step 3.2: Transfer governance from `governance_source` to `governance_escrow`.
+        cpi::spl::transfer_checked(
+            crate::GOVERNANCE_BOND,
+            mint_decimals,
+            TransferChecked {
+                source: governance_source,
+                destination: governance_escrow,
+                mint: governance_mint,
+                authority: asserter,
+                token_program,
+            },
+            &[],
+        )?;
+    }
+
+    // TODO: Emit an event for the request?
 
     Ok(())
 }
