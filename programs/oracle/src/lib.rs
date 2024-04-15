7863745be0f3cbb71fb6df4824a8c77e4742e61c
James Whaley <juicy66173@gmail.com>
2024-04-15 09:35:01 +0100

    Shorten dispute window for demo

--- programs/oracle/src/lib.rs
+++ programs/oracle/src/lib.rs
@@ -13,8 +13,8 @@ pub mod pda;
 pub mod processor;
 pub mod state;
 
-/// The duration of the dispute window (5 mins).
-pub const DISPUTE_WINDOW: i64 = 5 * 60; // 2 * 24 * 60 * 60;
+/// The duration of the dispute window (15 secs).
+pub const DISPUTE_WINDOW: i64 = 15; // 2 * 24 * 60 * 60;
 
 /// The duration of the voting window (5 mins).
 pub const VOTING_WINDOW: i64 = 5 * 60; // 1 * 24 * 60 * 60;
